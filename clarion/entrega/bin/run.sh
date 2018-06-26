80b1908b5b17e198a2d8e615530f093bf47e2be0
Leonardo De Oliveira Ramos - 171941 <ra171941@le27-11.grad.fee.unicamp.br>
2018-06-26 09:26:11 -0300

    clarion e soar

--- clarion/entrega/bin/run.sh
+++ clarion/entrega/bin/run.sh
@@ -3,6 +3,8 @@ echo "Running WS3D"
 java -jar "./ws3d/WorldServer3D.jar"&
 
 sleep 3
+java -jar "jSoar/DemoJSOAR.jar" &
 
+sleep 5
 echo "Running ClarionApp"
-./ClarionApp/ClarionApp.exe
+./ClarionApp/ClarionApp.exe 
